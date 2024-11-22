51cba9da8d47b3d3a549923b0540a5891bef9723
EdPritschKasper <dudukasperino@gmail.com>
2024-11-21 21:35:17 -0300

    Segunda Pagina e css do dropdown

--- scripts/login.js
+++ scripts/login.js
@@ -1,2 +1,9 @@
-import { API_BASE_URL } from "../../config/apiConfig.js";
-import { saveToLocalStorage } from "../utils/storage.js";
+import { API_BASE_URL } from "../config/apiConfig.js";
+import { saveToLocalStorage } from "../scripts/storage.js";
+
+
+document.getElementById("loginbotao").addEventListener("click",(event)=>{
+    event.preventDefault();
+    window.location.href="boards.html";
+
+})
